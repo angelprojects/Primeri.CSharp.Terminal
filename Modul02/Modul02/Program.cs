2bc97d474b36e5c25a76f3573a8bc369d15da11b
Angel Todorov <todorovproject@gmail.com>
2015-08-19 08:41:10 +0300

    Модул 02. Видео 04 - Работа със стрингове

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -10,11 +10,17 @@ namespace Modul02
 			//Дефиниране на променливи
 			int a = 0, b = 0;
 			int sum = 0;
+			string programe_name = "Модул02 - Примери";
+			string programe_version = "1.0";
 
 			//Събиране на числа
 			a = 7;
 			b = 4;
 			sum = a + b;
+
+			//За програмата
+			Console.WriteLine ( programe_name );
+			Console.WriteLine ( "Версия: " + programe_version + "\n\n" );
 
 			//Писане в конзола
 			Console.Write ( a );
@@ -47,6 +53,9 @@ namespace Modul02
 
 			Console.WriteLine ("\nХващане на остатък");
 			Console.WriteLine ( a % b);	//5, 1.5
+
+			Console.WriteLine ( programe_name );
+
 		}
 	}
 }
